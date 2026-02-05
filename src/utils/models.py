f072c7660abc863d3afcfebb76df1d799ed72212
Aadhya Purohit <rimi.pits@gmail.com>
2026-02-05 14:36:28 +0530

    added common columns

---
+++ src/utils/models.py
@@ -0,0 +1,50 @@
+
+     
+from collections.abc import Callable
+from enum import EnumType
+from functools import lru_cache
+ 
+from sqlalchemy import JSON, Boolean, Column, DateTime, func, text
+from sqlalchemy.dialects.postgresql import UUID
+ 
+from utils.datetime_utils import get_current_datetime
+ 
+ 
+class IsActiveColumn:
+    is_active = Column(Boolean, nullable=False, default=True, index=True, server_default=text("true"))
+ 
+ 
+class TimestampColumn:
+    created_at = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        default=get_current_datetime,
+        index=True,
+        server_default=func.now(),
+    )
+    updated_at = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        default=get_current_datetime,
+        onupdate=get_current_datetime,
+        server_default=func.now(),
+    )
+ 
+class AuditColumn:
+    created_by = Column("created_by", UUID(as_uuid=True), nullable=False)
+    updated_by = Column("updated_by", UUID(as_uuid=True), nullable=False, index=True)
+ 
+class ChangeLogColumn:
+    changes = Column("change_log", JSON, nullable=True)
+ 
+ 
+ 
+
+
+__all__ = [
+    "AuditColumn",
+    "ChangeLogColumn",
+    "IsActiveColumn",
+    "TimestampColumn",
+]
+ 
